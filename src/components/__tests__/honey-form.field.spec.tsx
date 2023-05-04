d7f75282bc875433ea32199ad24acf70e77c0077
Mykhailo Aliinyk <m.aliynik@gmail.com>
2023-05-04 12:27:25 +0100

    3.20.2v - added test case to work with radio elements

--- src/components/__tests__/honey-form.field.spec.tsx
+++ src/components/__tests__/honey-form.field.spec.tsx
@@ -28,4 +28,59 @@ describe('HoneyFormField component', () => {
 
     await waitFor(() => expect(onSubmit).toBeCalledWith({ product: 'apple' }));
   });
+
+  it('should submit form with correct gender value for unselected and selected radio inputs', async () => {
+    type Form = {
+      gender: string;
+    };
+
+    const onSubmit = jest.fn<Promise<void>, Form[]>();
+
+    const { getByTestId } = render(
+      <HoneyForm onSubmit={onSubmit}>
+        <HoneyFormField name="gender" value={null}>
+          {field => (
+            <fieldset name="gender">
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                data-testid="male"
+                onChange={field.props.onChange}
+              />
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+                data-testid="female"
+                onChange={field.props.onChange}
+              />
+              <input
+                type="radio"
+                name="gender"
+                value="other"
+                data-testid="other"
+                onChange={field.props.onChange}
+              />
+            </fieldset>
+          )}
+        </HoneyFormField>
+
+        <button type="submit" data-testid="save">
+          Save
+        </button>
+      </HoneyForm>
+    );
+
+    // case 1
+    fireEvent.click(getByTestId('save'));
+
+    await waitFor(() => expect(onSubmit).toBeCalledWith({ gender: null }));
+
+    // case 2
+    fireEvent.click(getByTestId('female'));
+    fireEvent.click(getByTestId('save'));
+
+    await waitFor(() => expect(onSubmit).toBeCalledWith({ gender: 'female' }));
+  });
 });
