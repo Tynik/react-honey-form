4a66f4aeb76c9ae8ef2c5a5db8aa4e1562fcfc99
Mykhailo Aliinyk <m.aliynik@gmail.com>
2023-09-02 21:54:18 +0100

    8.1.0v - added `undefined` type for dateFrom/dateTo types

--- src/form.types.ts
+++ src/form.types.ts
@@ -1,5 +1,5 @@
-type DateFrom = Date | null;
-type DateTo = Date | null;
+type DateFrom = Date | null | undefined;
+type DateTo = Date | null | undefined;
 
 export type CustomDateRangeForm<DateFromKey extends string, DateToKey extends string> = {
   [K in DateFromKey]: DateFrom;
